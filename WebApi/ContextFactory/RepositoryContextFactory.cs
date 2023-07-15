b3e6812c29a5c399da38e27ace2c868d4dfd04fe
otipz19 <tasitwork@gmail.com>
2023-07-15 15:42:37 +0300

    Created and configured db context

---
+++ WebApi/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace WebApi.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>();
+            builder.UseSqlServer(config.GetConnectionString("localhost"),
+                builder =>
+                {
+                    builder.MigrationsAssembly("WebApi");
+                });
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
