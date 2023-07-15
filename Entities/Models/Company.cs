41a6361572a5c39b7d2c653c4a2b01b915ed11ab
otipz19 <tasitwork@gmail.com>
2023-07-15 15:17:43 +0300

    Created Employee and Company models

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Company
+    {
+        [Column("CompanyId")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string? Name { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string? Address { get; set; }
+
+        public string? Country { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
