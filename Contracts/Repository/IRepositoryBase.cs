035139eca7af8ac43d17fa54ec391924d59daca8
otipz19 <tasitwork@gmail.com>
2023-07-15 16:15:19 +0300

    Created base repository

---
+++ Contracts/Repository/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts.Repository
+{
+    public interface IRepositoryBase<T>
+        where T : class
+    {
+        public IQueryable<T> GetAll(bool asNoTracking = false);
+
+        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression, bool asNoTracking = false);
+
+        public void Create(T entity);
+
+        public void Update(T entity);
+
+        public void Delete(T entity);
+    }
+}
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using Contracts.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
+        where T : class
+    {
+        protected readonly RepositoryContext _context;
+
+        protected RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> GetAll(bool asNoTracking = false)
+        {
+            var set = _context.Set<T>();
+            return asNoTracking ? set.AsNoTracking() : set;
+        }
+
+        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression, bool asNoTracking = false)
+        {
+            var set = _context.Set<T>().Where(expression);
+            return asNoTracking ? set.AsNoTracking() : set;
+        }
+    }
+}
